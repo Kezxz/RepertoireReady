22086c1a9e5f9b1d523d6aa32788ef7850e01619
abeffan <abeffan@gmail.com>
2026-02-22 15:39:46 -0600

    Simple piece_list UI

--- web/routes/pieces_routes.py
+++ web/routes/pieces_routes.py
@@ -1,6 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from app.piece_logic import PieceLibrary, Piece
 
 pieces_bp = Blueprint("pieces", __name__, url_prefix="/pieces")
+
+
+# Below can be deleted later. It's for testing
+library = PieceLibrary()
+library.add_piece(Piece(1, "Moonlight Sonata", "Beethoven", "Classical", "In Progress", 101))
+library.add_piece(Piece(2, "Clair de Lune", "Debussy", "Classical", "Ready", 102))
+library.add_piece(Piece(3, "Nocturne in E-flat Major", "Chopin", "Classical", "In Progress", 103))
+library.add_piece(Piece(4, "The Four Seasons", "Vivaldi", "Classical", "Ready", 104))
+
 
 
 @pieces_bp.get("/")
@@ -8,8 +18,4 @@ def pieces_home():
 
     # Placeholder
 
-    return (
-        "<h2>Pieces</h2>"
-        "<p>Placeholder page.</p>"
-        '<p><a href="/">Back to Home</a></p>'
-    )
+    return render_template("pieces_list.html", pieces = library.pieces)
